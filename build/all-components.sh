85ad1ef8b5c9e9827ba98ba8241e4ff862770379
Nagarajan, Raghavender <raghavender.nagarajan@intel.com>
2020-11-10 10:41:00 +0530

    added all-components build script

--- build/all-components.sh
+++ build/all-components.sh
@@ -2,18 +2,18 @@
 
 echo "Installing Foundational Security pre-reqs..."
 cd foundational-security/
-./fs-prereqs.sh -s
+chmod +x fs-prereq.sh;./fs-prereq.sh -s
 cd ..
 
 echo "Installing Workload Security pre-reqs"
 cd workload-security/
+chmod +x ws-prereq.sh
 
 #VM-C
-./ws-prereqs.sh -v
+./ws-prereq.sh -v
 
 #Container-Conf-Docker
-./ws-prereqs.sh -d
+./ws-prereq.sh -d
 
 #Container-Conf-CRIO
-./ws-prereqs.sh -c
-
+./ws-prereq.sh -c
